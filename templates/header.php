5eaa5dcaf55993f6163c577eeced4f67f93adfc4
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2021-10-07 13:26:40 -0600

    commit

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,22 @@
-<?php 
+<?php
 
+// require mySQL config template
 require('mySQLConfig.php');
 
+
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<!-- page header -->
+<head>
+  <title>Flight Booking</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <a href="index.php">
+    <h1>Title</h1>
+  </a>
+  <hr>
--- templates/mySQLConfig.php
+++ templates/mySQLConfig.php
@@ -1,6 +1,5 @@
-<?php 
+<?php
 
+// establish mySQL connection
 $pdo = new PDO("mysql:host=localhost;dbname=flightbooking", 'flightbooking', 'bookflights');
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-
-?>
