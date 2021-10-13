79c6e9fe01eba4442e0ea3685b9b7c60657cb417
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2021-10-13 13:27:28 -0600

    commit

--- bookFlight.php
+++ bookFlight.php
@@ -3,11 +3,16 @@
 // require header template
 require('templates/header.php');
 
+// set global array
+$seatsBooked = [];
+for ($i = 0; $i < 30; $i++) {
+  array_push($seatsBooked, ['A', 'B', 'C', 'D', 'E', 'F']);
+}
+
 // get seats available for flight selected
 if (isset($_GET['flightId'])) {
   $stmt = $pdo->query("SELECT * FROM flight_bookings WHERE flight_id = {$_GET['flightId']}");
-  $stmt->fetchAll();
-  // echo ("flight id is {$_GET['flightId']}");
+  $bookings = $stmt->fetchAll();
 }
 
 // form action
@@ -18,13 +23,22 @@ if (isset($_GET['flightId'])) {
 ?>
 
 <div class="seatSelection">
-  <p>Select a seat:</p>
+  <?php foreach ($bookings as $booking) {
+    unset($seatsBooked[$booking->row])
+    for ($row = 0; $row < sizeof($seatsBooked); $row++) {
+      if ($booking->row)
+    }
+  } ?>
+  <p>Select a row:</p>
+  <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+    <select name="row">
+      <option value="1">1</option>
+    </select>
 </div>
 <hr>
 <div class="personalInfo">
-  <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
-    <p>First name: <input type="text" name="firstName"></p>
-    <p>Last name: <input type="text" name="lastName"></p>
+  <p>First name: <input type="text" name="firstName"></p>
+  <p>Last name: <input type="text" name="lastName"></p>
 </div>
 <hr>
 <input type="submit" value="Book" name="bookButton">
--- details.php
+++ details.php
@@ -4,22 +4,22 @@ require('templates/header.php');
 
 // get schedule for route clicked
 if (isset($_GET['routeId'])) {
-  $stmt = $pdo->query("SELECT * FROM routeschedules WHERE route_id = {$_GET['routeId']} && capacity - seats_booked > 0");
-  $routeSchedule = $stmt->fetchAll();
+  $stmt = $pdo->query("SELECT * FROM flights WHERE route_id = {$_GET['routeId']} && capacity - seats_booked > 0");
+  $flights = $stmt->fetchAll();
 } else {
 }
 
-print_r($routeSchedule);
+print_r($flights);
 
 // form action
 if (isset($_POST['bookButton'])) {
-  header("Location: bookFlight.php?flightId={$_POST[flightId]}");
+  header("Location: bookFlight.php?flightId={$_POST['flightId']}");
 }
 
 ?>
 
 <h2><?= $_GET['origin'] ?> - <?= $_GET['destination'] ?></h2>
-<?php if (sizeof($routeSchedule) > 0) { ?>
+<?php if (sizeof($flights) > 0) { ?>
   <h3 class="scheduleHeading">Schedule:</h3>
   <table>
     <tr>
@@ -27,7 +27,7 @@ if (isset($_POST['bookButton'])) {
       <th>Time</th>
       <th>Seats Available</th>
     </tr>
-    <?php foreach ($routeSchedule as $flight) { ?>
+    <?php foreach ($flights as $flight) { ?>
       <tr>
         <td><?= $flight->date ?></td>
         <td><?= $flight->time ?></td>
