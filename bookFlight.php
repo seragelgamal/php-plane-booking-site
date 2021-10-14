251ae3e449a4cd25f602c7ba04a6c5f6b17ea0db
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2021-10-14 13:27:23 -0600

    Update bookFlight.php

--- bookFlight.php
+++ bookFlight.php
@@ -3,17 +3,30 @@
 // require header template
 require('templates/header.php');
 
-// set global array
-$seatsBooked = [];
-for ($i = 0; $i < 30; $i++) {
-  array_push($seatsBooked, ['A', 'B', 'C', 'D', 'E', 'F']);
-}
-
 // get seats available for flight selected
 if (isset($_GET['flightId'])) {
   $stmt = $pdo->query("SELECT * FROM flight_bookings WHERE flight_id = {$_GET['flightId']}");
   $bookings = $stmt->fetchAll();
 }
+
+// set global array to store all possible seats
+$columnLegend = ['A', 'B', 'C', 'D', 'E', 'F'];
+$seats = [];
+for ($row = 1; $row < 31; $row++) {
+  for ($column = 0; $column < 6; $column++) {
+    array_push($seats, $row . $columnLegend[$column]);
+  }
+}
+
+// set global array to store the seats that are already booked
+$seatsBooked = [];
+foreach ($bookings as $booking) {
+  array_push($seatsBooked, $booking->seat_row . $booking->seat_column);
+}
+print_r($seatsBooked);
+
+// take out booked seats from the $seats array
+$seats = array_diff($seats, $seatsBooked);
 
 // form action
 // if (isset($_POST['bookButton'])) {
@@ -23,25 +36,22 @@ if (isset($_GET['flightId'])) {
 ?>
 
 <div class="seatSelection">
-  <?php foreach ($bookings as $booking) {
-    unset($seatsBooked[$booking->row])
-    for ($row = 0; $row < sizeof($seatsBooked); $row++) {
-      if ($booking->row)
-    }
-  } ?>
-  <p>Select a row:</p>
+  <p>Select a seat from the following availablities:</p>
   <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
     <select name="row">
-      <option value="1">1</option>
+      <?php foreach ($seats as $seat) { ?>
+        <option value="<?= $seat ?>"><?= $seat ?></option>
+      <?php } ?>
     </select>
+    <!-- </div> -->
+    <hr>
+    <!-- <div class="personalInfo"> -->
+    <p>First name: <input type="text" name="firstName"></p>
+    <p>Last name: <input type="text" name="lastName"></p>
+
+    <hr>
+    <input type="submit" value="Book" name="bookButton">
+  </form>
 </div>
-<hr>
-<div class="personalInfo">
-  <p>First name: <input type="text" name="firstName"></p>
-  <p>Last name: <input type="text" name="lastName"></p>
-</div>
-<hr>
-<input type="submit" value="Book" name="bookButton">
-</form>
 
 <?php require('templates/footer.php'); ?>
