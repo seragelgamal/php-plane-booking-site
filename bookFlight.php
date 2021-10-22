edb6b81c87c22b1a47615c13bc9da27bf370b7d2
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2021-10-22 13:28:10 -0600

    Update bookFlight.php

--- bookFlight.php
+++ bookFlight.php
@@ -3,13 +3,58 @@
 // require header template
 require('templates/header.php');
 
+
+// set global form variables
+$seat = $row = $column = $firstName = $lastname = '';
+$firstNameErrors = $lastNameErrors = [];
+
+// form action
+if (isset($_POST['bookButton'])) {
+  
+
+  // separate seat row and column and store them
+  $seat = strlen($_POST['seat']) > 2 ? chunk_split($_POST['seat'], 2, ' ') : chunk_split($_POST['seat'], 1, ' ');
+  $row = strtok($seat, ' ');
+  $column = strtok(' ');
+
+  // validate first and last name
+  $firstName = $_POST['firstName'];
+  if (preg_match('~[0-9]~', $firstName)) {
+    array_push($firstNameErrors, 'First name can only contain letters');
+  }
+  if (strlen($firstName) > 255) {
+    array_push($firstNameErrors, 'First name can only be a maximum of 255 characters long');
+  }
+
+  $lastName = $_POST['lastName'];
+  if (preg_match('~[0-9]~', $lastName)) {
+    array_push($lastNameErrors, 'Last name can only contain letters');
+  }
+  if (strlen($lastName) > 255) {
+    array_push($lastNameErrors, 'Last name can only be a maximum of 255 characters long');
+  }
+
+  if (sizeof($firstNameErrors) == 0 && sizeof($lastNameErrors) == 0) {
+    // format first and last name
+    $firstName = trim($firstName);
+    $firstName = ucfirst($firstName);
+
+    $lastName = trim($lastName);
+    $lastName = ucfirst($lastName);
+
+    // push name and seat to server
+    $stmt = $pdo->prepare('INSERT INTO flight_bookings(first_name, last_name, flight_id, seat_row, seat_column) VALUES(:flightId, :firstName, :lastName, :seatRow, :seatColumn)');
+    $stmt->execute(['firstName' => $firstName, 'lastName' => $lastName, 'flightId' => $_GET['flightId'], 'seatRow' => $row, 'seatColumn' => $column]);
+  }
+}
+
 // get seats available for flight selected
 if (isset($_GET['flightId'])) {
   $stmt = $pdo->query("SELECT * FROM flight_bookings WHERE flight_id = {$_GET['flightId']}");
   $bookings = $stmt->fetchAll();
 }
 
-// set global array to store all possible seats
+// set global array '$seats' to store all possible seats
 $columnLegend = ['A', 'B', 'C', 'D', 'E', 'F'];
 $seats = [];
 for ($row = 1; $row < 31; $row++) {
@@ -23,57 +68,9 @@ $seatsBooked = [];
 foreach ($bookings as $booking) {
   array_push($seatsBooked, $booking->seat_row . $booking->seat_column);
 }
-// print_r($seatsBooked);
 
 // take out booked seats from the $seats array
 $seats = array_diff($seats, $seatsBooked);
-
-  $firstNameErrors = $lastNameErrors = [];
-
-// form action
-if (isset($_POST['bookButton'])) {
-  $seat = $row = $column = $firstName = $lastname = '';
-
-  // validate first and last name
-  $firstName = $_POST['firstName'];
-  // preg_match()
-
-  // if (gettype($firstName) != 'string') {
-  //   echo('not a string');
-  // }
-  // for ($letter = 0; $letter < strlen($firstName); $letter++) {
-  //   if (gettype($firstName[$letter]) != 'string') {
-  //     array_push($firstNameErrors, 'First name can only contain letters');
-  //   }
-  // }
-  trim($firstName);
-  ucfirst($firstName);
-  if (strlen($firstName) > 255) {
-    array_push($firstNameErrors, 'First name can only be a maximum of 255 characters long');
-  }
-
-  $lastName = $_POST['lastName'];
-  for ($letter = 0; $letter < strlen($lastName); $letter++) {
-    if (gettype($lastName[$letter]) != 'string') {
-      array_push($lastNameErrors, 'Last name can only contain letters');
-    }
-  }
-  trim($lastName);
-  ucfirst($lastName);
-  if (strlen($lastName) > 255) {
-    array_push($lastNameErrors, 'Last name can only be a maximum of 255 characters long');
-  }
-
-  if (sizeof($firstNameErrors) > 0 || sizeof($lastNameErrors) > 0) {
-    print_r($firstNameErrors);
-    print_r($lastNameErrors);
-  }
-
-  // separate seat row and column
-  $seat = strlen($_POST['seat']) > 2 ? chunk_split($_POST['seat'], 2, ' ') : chunk_split($_POST['seat'], 1, ' ');
-  $row = strtok($seat, ' ');
-  $column = strtok(' ');
-}
 
 ?>
 
@@ -82,7 +79,7 @@ if (isset($_POST['bookButton'])) {
   <form action="<?= htmlspecialchars("{$_SERVER['PHP_SELF']}?flightId={$_GET['flightId']}") ?>" method="post">
     <select name="seat">
       <?php foreach ($seats as $seat) { ?>
-        <option value="<?= $seat ?>"><?= $seat ?></option>
+        <option value="<?= $seat ?>"<?php if ((isset($row) && isset($column)) && $seat == "$row$column") {echo('selected');} ?>><?= $seat ?></option>
       <?php } ?>
     </select>
     <hr>
