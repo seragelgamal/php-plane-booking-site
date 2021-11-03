5e2138d56e00d6bf2c614eb92f326ee05ad631e1
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2021-11-03 13:28:02 -0600

    commit

--- bookFlight.php
+++ bookFlight.php
@@ -10,12 +10,21 @@ if ($stmt->rowCount() == 0) {
   header('Location: index.php');
 }
 
-// set global form variables
-$seat = $row = $column = $firstName = $lastname = '';
-$firstNameErrors = $lastNameErrors = [];
+// set global form and seat info variables
+$seat = $row = $column = $firstName = $lastName = '';
+$seatErrors = $firstNameErrors = $lastNameErrors = [];
+$availabilityInfo = '';
 
 // form action
 if (isset($_POST['bookButton'])) {
+  // make sure a seat is selected
+  if (!isset($_POST['seat'])) {
+    // if a seat isnt selected: push seat error
+    array_push($seatErrors, 'Seat selection is required');
+  } else {
+    
+  }
+
   // separate seat row and column and store them
   $seat = strlen($_POST['seat']) > 2 ? chunk_split($_POST['seat'], 2, ' ') : chunk_split($_POST['seat'], 1, ' ');
   $row = strtok($seat, ' ');
@@ -65,7 +74,8 @@ if (isset($_POST['bookButton'])) {
     $lastName = trim($lastName);
     $lastName = ucfirst($lastName);
 
-    // push name and seat to server$stmt = $pdo->prepare('INSERT INTO flight_bookings (first_name, last_name, flight_id, seat_row, seat_column) VALUES (:firstName, :lastName, :flightId, :seatRow, :seatColumn)');
+    // push name and seat to server
+    $stmt = $pdo->prepare('INSERT INTO flight_bookings (first_name, last_name, flight_id, seat_row, seat_column) VALUES (:firstName, :lastName, :flightId, :seatRow, :seatColumn)');
     $stmt->execute(['firstName' => $firstName, 'lastName' => $lastName, 'flightId' => $_GET['flightId'], 'seatRow' => $row, 'seatColumn' => $column]);
 
     // update seats booked in database
@@ -79,16 +89,18 @@ if (isset($_POST['bookButton'])) {
 
 // get seat layout for flight selected, as well as booked seats
 if (isset($_GET['flightId'])) {
-  $stmt = $pdo->query("SELECT number_of_rows, number_of_columns FROM flights WHERE id = {$_GET['flightId']}");
-  $rows = $stmt->fetch()->number_of_rows;
-  // print_r($rows);
+  $stmt = $pdo->query("SELECT number_of_rows, number_of_columns, seats_booked FROM flights WHERE id = {$_GET['flightId']}");
+  $availabilityInfo = $stmt->fetch();
 
   $stmt = $pdo->query("SELECT * FROM flight_bookings WHERE flight_id = {$_GET['flightId']}");
   $bookings = $stmt->fetchAll();
+  // print_r($bookings);
 }
+// print_r($availabilityInfo);
+// print_r($availabilityInfo->seats_booked);
 
 // set global array '$seats' to store all possible seats
-$columnLegend = ['A', 'B', 'C', 'D', 'E', 'F'];
+$columnLegend = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K'];
 $seats = [];
 for ($seatRow = 1; $seatRow < 31; $seatRow++) {
   for ($seatColumn = 0; $seatColumn < 6; $seatColumn++) {
@@ -107,13 +119,26 @@ $seats = array_diff($seats, $seatsBooked);
 ?>
 
 <div class="seatSelection">
-  <p>Select a seat from the following availablities:</p>
+  <p class="seatSelection">Select a seat from the following availablities:</p>
+  <p class="errors"><?php foreach ($seatErrors as $error) {
+                      echo ("$error <br>");
+                    } ?></p>
   <form action="<?= htmlspecialchars("{$_SERVER['PHP_SELF']}?flightId={$_GET['flightId']}") ?>" method="post">
-    <select name="seat">
+    <!-- <select name="seat">
       <?php foreach ($seats as $seat) { ?>
         <option value="<?= $seat ?>" <?php if ($seat == "$row$column") { ?> selected <?php } ?>><?= $seat ?></option>
       <?php } ?>
-    </select>
+    </select> -->
+    <?php for ($r = 1; $r < ($availabilityInfo->number_of_rows + 1); $r++) { ?>
+      <div class="seatSelection">
+        <p><?= $r ?></p>
+        <?php for ($c = 0; $c < ($availabilityInfo->number_of_columns); $c++) { ?>
+          <input type="radio" name="seat" value="<?= "$r{$columnLegend[$c]}" ?>" <?php foreach ($bookings as $booking) {
+                                                                                    if ("$r{$columnLegend[$c]}" == "{$booking->seat_row}{$booking->seat_column}") { ?> disabled <?php }
+                                                                                                                                                                            } ?>><?= $columnLegend[$c] ?>
+        <?php } ?>
+      </div>
+    <?php } ?>
     <hr>
     <p>First name: <input type="text" name="firstName" value='<?php if (isset($_POST['firstName'])) {
                                                                 echo ($_POST['firstName']);
